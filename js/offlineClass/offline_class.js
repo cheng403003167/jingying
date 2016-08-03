e1f56db461c7feb9e696c8d3f3a71f4d56e12ef8
ZhuQinPeng <zhu84735335@sina.com>
2016-08-03 21:04:07 +0800

    z1.0

--- js/offlineClass/offline_class.js
+++ js/offlineClass/offline_class.js
@@ -1,11 +1,10 @@
 $(function(){
 	var page = 0,lass = '';
 	$.ajax({
-		type:"get",
 		url:"../PHP/offlineClass/classlist.php",
-		dataType:"JSON",
+		// dataType:"JSON",
+		cathe:false,
 		success:function(data){
-			console.log(data)
 			$(".class_num span").text(data.length);
 			var zheng = parseInt(data.length/15);
 			if(data.length%15>0){
