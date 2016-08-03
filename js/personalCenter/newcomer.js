97ac4a72eb898d01cac3a6fea75f60a830d83d63
chan <403003167@qq.com>
2016-08-03 20:32:35 +0800

    a

--- js/personalCenter/newcomer.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	var _len = $(".sideBar").find("p").length;
-	for (var i = 0; i < _len; i++) {
-		$(".sideBar p").eq(i).on("click", function() {
-			$(".sideBar").find("p").removeClass("selected");
-			$(this).addClass("selected");
-		});
-	}
-});
